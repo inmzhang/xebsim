c849ebe779b1b018ba702fb94a057f81bef77c63
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2023-09-21 13:30:57 +0800

    use step simulation for performance

--- noise_injection.py
+++ noise_injection.py
@@ -1,7 +1,7 @@
 """Use single qubit XEB to benchmark the effect of noise injection."""
 
 import itertools
-from typing import Sequence, Tuple, Iterable
+from typing import Sequence, Tuple, Iterable, Callable
 
 import numpy as np
 import cirq
@@ -46,6 +46,69 @@ class MixedNoiseModel(cirq.NoiseModel):
             )
             return [moment, coherent_noise_moment, incoherent_noise_moment]
         return moment
+
+
+def step_simulate_with_noise_model(
+        circuits: Sequence[cirq.Circuit],
+        noise_model: cirq.NoiseModel,
+        *,
+        cycle_depths: Sequence[int],
+        cycle_to_circuit_depth_func: Callable[[int], int] = lambda x: x + 1,
+) -> Tuple[float, float]:
+    pure_sim = cirq.Simulator()
+    noisy_sim = cirq.DensityMatrixSimulator(noise=noise_model)
+    qubit = list(circuits[0].all_qubits())[0]
+
+    records = []
+    for circuit_i, circuit in enumerate(circuits):
+        pure_step_results = [np.abs(res.state_vector()) ** 2 for res in pure_sim.simulate_moment_steps(circuit)]
+        noisy_step_results = [
+            np.abs(np.diag(res.density_matrix()))
+            for res in noisy_sim.simulate_moment_steps(circuit)
+        ]
+
+        for cycle_depth in sorted(cycle_depths):
+            circuit_depth = cycle_to_circuit_depth_func(cycle_depth)
+            assert circuit_depth <= len(circuit)
+            pure_probs = pure_step_results[circuit_depth - 1]
+            noisy_probs = noisy_step_results[circuit_depth - 1]
+
+            # Save the results
+            records += [{
+                'circuit_i': circuit_i,
+                'cycle_depth': cycle_depth,
+                'circuit_depth': circuit_depth,
+                'pure_probs': pure_probs,
+                'sampled_probs': noisy_probs,
+            }]
+    for record in records:
+        e_u = np.sum(record['pure_probs']**2)
+        u_u = np.sum(record['pure_probs']) / 2
+        m_u = np.sum(record['pure_probs'] * record['sampled_probs'])
+        record.update(
+            e_u=e_u,
+            u_u=u_u,
+            m_u=m_u,
+        )
+
+    df = pd.DataFrame(records)
+    df['y'] = df['m_u'] - df['u_u']
+    df['x'] = df['e_u'] - df['u_u']
+
+    df['numerator'] = df['x'] * df['y']
+    df['denominator'] = df['x'] ** 2
+
+    def per_cycle_depth(df):
+        fid_lsq = df['numerator'].sum() / df['denominator'].sum()
+        return pd.Series({'fidelity': fid_lsq})
+
+    fids = df.groupby(['cycle_depth']).apply(per_cycle_depth).reset_index()
+    fids['qubit'] = [qubit] * len(fids)
+
+    fit_df = fit_exponential_decays(fids)
+
+    fit_row = fit_df.iloc[0]
+    return 1 - fit_row['layer_fid'], fit_row['layer_fid_std']
 
 
 def simulate_with_noise_model(
